50ac1c76e2af3373ae015ee5c7b72014130db1c6
ChiatanyaNathSingh <mchaitanyanathsingh@gmail.com>
2022-07-17 13:22:59 +0530

    First Commit

--- crud/admin.py
+++ crud/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import Image
 from django.utils.safestring import mark_safe
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'ImgName', 'image', 'short_description')
     search_fields =['ImgName']
     ordering = ['id']
     readonly_fields = ['image', 'short_description']
+    save_as = True
+    save_on_top = True
 
     def image(self, obj):
         return mark_safe('<img src="{url}" width="150" height="150"/>'.format(
--- gallery/settings.py
+++ gallery/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
 ]
 
 MIDDLEWARE = [
